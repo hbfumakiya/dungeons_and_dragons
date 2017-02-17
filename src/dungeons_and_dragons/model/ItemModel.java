03c7c4e50dc91a390e867f0f88277dcde37c268f
kansaraurmil55 <User@User-pc>
2017-02-18 00:11:42 +0530

    Item Model setter getter

--- src/dungeons_and_dragons/model/ItemModel.java
+++ src/dungeons_and_dragons/model/ItemModel.java
@@ -1,59 +1,89 @@
 package dungeons_and_dragons.model;
+
 /**
- * Item class to store all items.
- * eg Helmet,Ring
+ * Item class to store all items. eg Helmet,Ring
  * 
  * @author : Urmil Kansara
  * 
  * 
  */
 public class ItemModel {
-	
+
 	/**
 	 * ID and Name of items
 	 * 
 	 * @type : Integer
-	 *        
+	 * 
 	 */
 	private int item_id;
-	
+
 	/**
 	 * Name of items
 	 * 
 	 * @type :String
 	 */
 	private String item_name;
-	
+
 	/**
 	 * Point of item.
 	 * 
 	 * @type : Integer
 	 */
-	
+
 	private int item_point;
-	
+
 	/**
 	 * Add ability by selecting item.
 	 *
-	 *@type : AbilityScores
+	 * @type : AbilityScores
 	 */
+
+	/**
+	 * Add types of item
+	 * 
+	 * type : String
+	 */
+	private String item_type;
+
+	/**
+	 * Add types of item
+	 * 
+	 * type : String
+	 */
+	private String item_ability;
 	
-	private AbilityScoresModel ability_scores ;
-	
-	
-	
-	public ItemModel(int item_id,
-			String item_name,
-			int item_point) 
-	{
+	/**
+	 * 
+	 * default Constructor
+	 */
+	public ItemModel() {
+
+	}
+
+	/**
+	 * This constructor sets id,name, point, type and ability;
+	 * 
+	 * @param item_id
+	 *            Id of item
+	 * @param item_name
+	 *            Name of Item
+	 * @param item_point
+	 *            Point of Item
+	 * @param item_type
+	 *            Type of item
+	 * @param item_ability
+	 *            ABility of item
+	 */
+
+	public ItemModel(int item_id, String item_name, int item_point, String item_type, String item_ability) {
 		// TODO Auto-generated constructor stub
 		this.item_id = item_id;
 		this.item_name = item_name;
 		this.item_point = item_point;
-		ability_scores = new AbilityScoresModel();
-		
-	}
+		this.item_type = item_type;
+		this.item_ability = item_ability;
 
+	}
 
 	/**
 	 * @return the item_id
@@ -62,14 +92,12 @@ public class ItemModel {
 		return item_id;
 	}
 
-
 	/**
 	 * @return the item_name
 	 */
 	public String getItem_name() {
 		return item_name;
 	}
-
 
 	/**
 	 * @return the item_point
@@ -79,9 +107,17 @@ public class ItemModel {
 	}
 
 	/**
-	 * @return the ability_scores
+	 * @return the item_type
 	 */
-	public AbilityScoresModel getAbility_scores() {
-		return ability_scores;
+	public String getItem_type() {
+		return item_type;
 	}
+
+	/**
+	 * @return the item_ability
+	 */
+	public String getItem_ability() {
+		return item_ability;
+	}
+
 }
