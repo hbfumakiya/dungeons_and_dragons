56dae2789b68a06e1607f0fe9609bbe56d4bd4ec
mihir <mihir@Mihir-Laptop>
2017-02-17 11:46:35 -0500

    project setup

---
+++ src/dungeons_and_dragons/model/Game.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dungeons_and_dragons.model;
+
+/**
+ * @author mihir
+ *
+ */
+public class Game {
+
+	/**
+	 * 
+	 */
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
