39733ed01795e633d0ad57b886e0fc623fbb0848
chauhnshahida <chauhanshahida@gmail.com>
2017-02-20 14:41:45 -0500

    test

---
+++ src/test/basictest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dungeons_and_dragons.model.CharacterModel;
+
+/**
+ * Check basic values of  character
+ * 
+ * @author : Shahida Chauhan
+ * 
+ * 
+ */
+
+public class basictest {
+	@Test
+	public void basicValueTest()
+	{	
+		//Given
+		int character_id =1;
+		String character_name="Bean";
+		int strength=10;
+		
+		//When
+		CharacterModel newcharacter=new CharacterModel(character_id,character_name,strength);
+		
+		//Then
+		assertEquals(newcharacter.getCharacter_name(),character_name);
+		assertEquals(newcharacter.getCharacter_id(),character_id);
+		assertEquals(newcharacter.getStrength(),strength);
+
+	}
+
+}
