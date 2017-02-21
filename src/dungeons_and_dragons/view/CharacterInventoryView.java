bba6804e6ec45813bd458ff60c21974a49958e30
mihir <mihir@Mihir-Laptop>
2017-02-20 23:37:49 -0500

    create inventory view

---
+++ src/dungeons_and_dragons/view/CharacterInventoryView.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package dungeons_and_dragons.view;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Mihir Pujara
+ *
+ */
+public class CharacterInventoryView extends JFrame {
+
+	public CharacterInventoryView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
