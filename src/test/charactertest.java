44d593e4ce512ba095c519e7f492233eddaf9136
chauhnshahida <chauhanshahida@gmail.com>
2017-02-22 14:20:56 -0500

    character testcase

---
+++ src/test/charactertest.java
@@ -0,0 +1,92 @@
+package test;
+
+import org.junit.Test;
+
+
+/**
+ * Character Test
+ * 
+ * @author : Shahida Chauhan
+ * 
+ * 
+ */
+
+public class charactertest {
+	/**
+	 * ABILITY SCORE AND MODIFIER
+	 *  
+	 * 4D6 rolled 6times and set modifier according to score
+	 * 
+	 * [4D6 + modifier]*Level
+	 * 
+	 * Strength (Str)
+	 * Dexterity (Dex)
+	 * Constitution (Con)
+	 * Intelligence (Int)
+	 * Wisdom (Wis)
+	 * Charisma (Cha)
+	 */
+	@Test
+
+	public void testAbilityAndModifier()
+	{
+		//Given
+		//When
+		//Then
+		
+	}
+	
+	/**
+	 * HIT POINTS
+	 * [10 + his Constitution modifier]*Level
+	 */
+	@Test
+	public void testHitPoints()
+	{
+		//Given
+		//When
+		//Then
+		
+	}
+	/**
+	 * ARMOR CLASS(DEX+WORN ARMOR)
+	 * 
+	 * [D10+ARMOR BONUS(RANGES FROM +1 TO +5)+DEXTIRITY]*Level
+	 */
+	@Test
+	public void testArmorClass()
+	{
+		//Given
+		//When
+		//Then
+		
+	}
+	/**
+	 * ATTACK BONUS(level+str/dex)
+	 * 
+	 * [+1/Level + str/dex]*Level
+	 * 
+	 */
+	@Test
+	public void testAttackBonus()
+	{
+		//Given
+		//When
+		//Then
+		
+	}
+	/**
+	 * DAMAGE BONUS(STR ONLY FOR MELEE WEAPON)
+	 * [str]*Level
+	 */
+	@Test
+	public void testDamageBonus()
+	{
+		//Given
+		//When
+		//Then
+		
+	}
+
+
+}
