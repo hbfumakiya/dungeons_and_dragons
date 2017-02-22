10aef42263941468cb17099e7762eb97c4e3d916
Hirangi <Hirangi@lenovo>
2017-02-22 02:35:27 -0500

    backpack

--- src/dungeons_and_dragons/view/CharacterView.java
+++ src/dungeons_and_dragons/view/CharacterView.java
@@ -1,14 +1,17 @@
 package dungeons_and_dragons.view;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -82,6 +85,8 @@ public class CharacterView extends JFrame implements Observer, View {
 	 * @type JPanel
 	 */
 	public JPanel sub_panel;
+	public JPanel button_panel;
+	public JPanel jlist_panel;
 
 	/**
 	 * this variable used for list panel
@@ -137,11 +142,12 @@ public class CharacterView extends JFrame implements Observer, View {
 	 * @type JButton
 	 */
 	public JButton back;
+	public JFrame frame;
 
 	public JButton add;
 	private ArrayList<ItemModel> items;
 	private ArrayList<ItemModel> backpack;
-	
+
 	public CharacterView() {
 
 		this.items = new ArrayList<ItemModel>();
@@ -167,150 +173,129 @@ public class CharacterView extends JFrame implements Observer, View {
 	 */
 	private void initializeWindow() {
 
-		// set window title
+		// frame=new JFrame("Character window");
 		this.setTitle(this.window_title);
+		this.setPreferredSize(new Dimension(400, 600));
+		this.setResizable(false);
+		this.setLayout(null);
 
-		main_panel = new JPanel();
-		main_panel.setLayout(new BoxLayout(main_panel, BoxLayout.PAGE_AXIS));
-		main_panel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(main_panel);
-
-		sub_panel = new JPanel();
-		sub_panel.setLayout(new BoxLayout(sub_panel, BoxLayout.LINE_AXIS));
-		sub_panel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		sub_panel.setMaximumSize(new Dimension(300, 1000));
-
-		main_panel.add(sub_panel);
-
-		list_panel = new JPanel();
-		list_panel.setLayout(new GridLayout(15, 2, 5, 5));
-		list_panel.setMaximumSize(new Dimension(400, 450));
-
-		sub_panel.add(list_panel);
-
-		// character name
 		charactername_label = new JLabel("Enter a Name");
-		list_panel.add(charactername_label);
-
+		charactername_label.setBounds(10, 10, 100, 25);
+		this.add(charactername_label);
 		charactername_textfield = new JTextField();
-		charactername_textfield.setPreferredSize(new Dimension(50, 40));
-		charactername_textfield.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(charactername_textfield);
+		charactername_textfield.setBounds(150, 10, 150, 25);
+		this.add(charactername_textfield);
 
 		// helmet
-
 		additem_label = new JLabel("Select Helmet");
-		list_panel.add(additem_label);
+		additem_label.setBounds(10, 40, 100, 25);
+		this.add(additem_label);
 		Object[] helmet = this.items.stream().filter(p -> p.getItem_type().equals(Game_constants.HELMET)).toArray();
 		helmet_combobox = new JComboBox(helmet);
 		if (helmet.length > 0)
 			helmet_combobox.setRenderer(new ItemRenderer());
-		helmet_combobox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(helmet_combobox);
+		helmet_combobox.setBounds(150, 40, 150, 25);
+		this.add(helmet_combobox);
 
 		// armer
 
 		additem_label = new JLabel("Select Armer");
-		list_panel.add(additem_label);
+		additem_label.setBounds(10, 70, 100, 25);
+		this.add(additem_label);
 		Object[] armer = this.items.stream().filter(p -> p.getItem_type().equals(Game_constants.ARMOR)).toArray();
 		armer_combobox = new JComboBox(armer);
 		if (armer.length > 0)
 			armer_combobox.setRenderer(new ItemRenderer());
-		armer_combobox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(armer_combobox);
+		armer_combobox.setBounds(150, 70, 150, 25);
+		this.add(armer_combobox);
 
 		// shield
 
 		additem_label = new JLabel("Select Shield");
-		list_panel.add(additem_label);
+		additem_label.setBounds(10, 100, 100, 25);
+		this.add(additem_label);
 		Object[] shields = this.items.stream().filter(p -> p.getItem_type().equals(Game_constants.SHIELD)).toArray();
 		shield_combobox = new JComboBox(shields);
 		if (shields.length > 0)
 			shield_combobox.setRenderer(new ItemRenderer());
-		shield_combobox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(shield_combobox);
+		shield_combobox.setBounds(150, 100, 150, 25);
+		this.add(shield_combobox);
 
 		// ring
 
 		additem_label = new JLabel("Select Ring");
-		list_panel.add(additem_label);
+		additem_label.setBounds(10, 130, 100, 25);
+		this.add(additem_label);
 		Object[] rings = this.items.stream().filter(p -> p.getItem_type().equals(Game_constants.RING)).toArray();
 		ring_combobox = new JComboBox(rings);
 		if (rings.length > 0)
 			ring_combobox.setRenderer(new ItemRenderer());
-		ring_combobox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(ring_combobox);
+		ring_combobox.setBounds(150, 130, 150, 25);
+		this.add(ring_combobox);
 
 		// belt
 
 		additem_label = new JLabel("Select Belt");
-		list_panel.add(additem_label);
+		additem_label.setBounds(10, 160, 100, 25);
+		this.add(additem_label);
 		Object[] belts = this.items.stream().filter(p -> p.getItem_type().equals(Game_constants.BELT)).toArray();
 		belt_combobox = new JComboBox(belts);
 		if (belts.length > 0)
 			belt_combobox.setRenderer(new ItemRenderer());
-		belt_combobox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(belt_combobox);
+		belt_combobox.setBounds(150, 160, 150, 25);
+		this.add(belt_combobox);
 
 		// boot
 
 		additem_label = new JLabel("Select Boot");
-		list_panel.add(additem_label);
+		additem_label.setBounds(10, 190, 100, 25);
+		this.add(additem_label);
 		Object[] boots = this.items.stream().filter(p -> p.getItem_type().equals(Game_constants.BOOTS)).toArray();
 		boot_combobox = new JComboBox(boots);
 		if (boots.length > 0)
 			boot_combobox.setRenderer(new ItemRenderer());
-		boot_combobox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(boot_combobox);
+		boot_combobox.setBounds(150, 190, 150, 25);
+		this.add(boot_combobox);
 
 		// weapon
 
 		additem_label = new JLabel("Select Weapon");
-		list_panel.add(additem_label);
+		additem_label.setBounds(10, 220, 100, 25);
+		this.add(additem_label);
 		Object[] weapon = this.items.stream().filter(p -> p.getItem_type().equals(Game_constants.WEAPON)).toArray();
 		weapon_combobox = new JComboBox(weapon);
 		if (weapon.length > 0)
 			weapon_combobox.setRenderer(new ItemRenderer());
-		weapon_combobox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		list_panel.add(weapon_combobox);
-
-		// list= new JList<String>(names);
-		// list.setAlignmentX(Component.LEFT_ALIGNMENT);
-		// list.setPreferredSize(new Dimension(200, 200));
-		// list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-		// list_panel.add(new JScrollPane(list));
+		weapon_combobox.setBounds(150, 220, 150, 25);
+		this.add(weapon_combobox);
 
 		level_label = new JLabel("Enter Level");
-		list_panel.add(level_label);
+		level_label.setBounds(10, 250, 100, 25);
+		this.add(level_label);
 
 		level_textfield = new JTextField();
 		level_textfield.setPreferredSize(new Dimension(50, 40));
-		level_textfield.setAlignmentY(LEFT_ALIGNMENT);
-		list_panel.add(level_textfield);
+		level_textfield.setBounds(150, 250, 150, 25);
+		this.add(level_textfield);
 
 		backpack_label = new JLabel("BackPack");
-		list_panel.add(backpack_label);
+		backpack_label.setBounds(10, 280, 100, 25);
+		this.add(backpack_label);
 
-		String[] backpack=new String[this.items.size()];
-		for(int i=0;i<this.items.size();i++){
-			backpack[i]=this.items.get(i).getItem_name();
+		String[] backpack = new String[this.items.size()];
+		for (int i = 0; i < this.items.size(); i++) {
+			backpack[i] = this.items.get(i).getItem_name();
 		}
-		backpack_combobox = new JComboBox<String>(backpack);
-		backpack_combobox.setAlignmentX(LEFT_ALIGNMENT);
-		list_panel.add(backpack_combobox);
-
-		add = new JButton("Add");
-		list_panel.add(add);
+		
 		
 		back = new JButton("Back");
-		list_panel.add(back);
+		back.setBounds(20, 355, 80, 25);
+		this.add(back);
 
 		save = new JButton("Save");
-		list_panel.add(save);
-
-		// set minimum size of frame
-		this.setPreferredSize(new Dimension(300, 600));
-
+		save.setBounds(110, 355, 80, 25);
+		this.add(save);
+		
 		// Display the window.
 		this.pack();
 		this.setLocationRelativeTo(null);
@@ -325,8 +310,8 @@ public class CharacterView extends JFrame implements Observer, View {
 	@Override
 	public void setActionListener(ActionListener actionListener) {
 		// TODO Auto-generated method stub
-		this.save.addActionListener(actionListener);
-		this.back.addActionListener(actionListener);
+		 this.save.addActionListener(actionListener);
+		 this.back.addActionListener(actionListener);
 	}
 
 	class ItemRenderer extends BasicComboBoxRenderer {
