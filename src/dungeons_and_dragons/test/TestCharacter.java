d646ede281cc3389c0129331510aec304ccf9b48
chauhnshahida <chauhanshahida@gmail.com>
2017-03-03 00:07:35 -0500

    test

--- src/dungeons_and_dragons/test/TestCharacter.java
+++ src/dungeons_and_dragons/test/TestCharacter.java
@@ -1,6 +1,5 @@
 package dungeons_and_dragons.test;
 import java.util.ArrayList;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,6 +16,259 @@ import dungeons_and_dragons.model.ItemModel;
 
 
 public class TestCharacter {
+	
+	
+	/**
+	 * Test character’s abilities according to items worn 
+	 */
+	@Test
+	public void testAbilityAccordingToItemWornHelmetI()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.HELMET;
+		String item_ability = Game_constants.INTELLIGENCE;
+		int item_point=1;
+		int intelligence=11;
+		
+		CharacterModel itemcharacter=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+		
+		itemcharacter.setItems(items_cha);
+		itemcharacter.getRawAbilityScores().setIntelligence(10);
+		
+		itemcharacter.calculateModifires();
+
+		Assert.assertEquals(itemcharacter.getAbilityScores().getIntelligence(), intelligence);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornHelmetW()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.HELMET;
+		String item_ability = Game_constants.WISDOM;
+		int item_point=1;
+		int wisdom=11;
+	
+		CharacterModel itemcharacter=new CharacterModel();
+
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+	
+
+		itemcharacter.setItems(items_cha);
+		itemcharacter.getRawAbilityScores().setWisdom(10);
+		
+		itemcharacter.calculateModifires();
+			
+		Assert.assertEquals(itemcharacter.getAbilityScores().getWisdom(),wisdom);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornRingS()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.RING;
+		String item_ability = Game_constants.STRENGTH;
+		int item_point=1;
+		int strength=11;
+		
+		CharacterModel itemcharacter=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+		
+		itemcharacter.setItems(items_cha);
+		itemcharacter.getRawAbilityScores().setstrength(10);
+		
+		itemcharacter.calculateModifires();
+			
+		Assert.assertEquals(itemcharacter.getAbilityScores().getStraight(),strength);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornRingC()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.RING;
+		String item_ability = Game_constants.CONSTITUTION;
+		int item_point=1;
+		int constitution=11;
+		
+		CharacterModel itemcharacter=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> itemscha = new ArrayList<ItemModel>();
+		itemscha.add(item);
+		
+
+		itemcharacter.setItems(itemscha);
+		itemcharacter.getRawAbilityScores().setConstitution(10);
+		
+		itemcharacter.calculateModifires();
+			
+		Assert.assertEquals(itemcharacter.getAbilityScores().getConstitution(),constitution);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornRingW()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.RING;
+		String item_ability = Game_constants.WISDOM;
+		int item_point=1;
+		int wisdom=11;
+		
+		CharacterModel item_character=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+
+		item_character.setItems(items_cha);
+		item_character.getRawAbilityScores().setWisdom(10);
+		
+		item_character.calculateModifires();
+			
+		Assert.assertEquals(item_character.getAbilityScores().getWisdom(),wisdom);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornRingCh()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.RING;
+		String item_ability = Game_constants.CHARISMA;
+		int item_point=1;
+		int charisma=11;
+		
+		CharacterModel item_character=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+		
+		item_character.setItems(items_cha);
+		item_character.getRawAbilityScores().setCharisma(10);
+		
+
+		item_character.calculateModifires();
+		
+			
+		Assert.assertEquals(item_character.getAbilityScores().getCharisma(),charisma);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornBeltS()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.BELT;
+		String item_ability = Game_constants.STRENGTH;
+		int item_point=1;
+		int strength=11;
+		
+		CharacterModel item_character=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+		
+
+
+		item_character.setItems(items_cha);
+		item_character.getRawAbilityScores().setstrength(10);
+		
+
+		item_character.calculateModifires();
+		
+		
+				
+		Assert.assertEquals(item_character.getAbilityScores().getStraight(),strength);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornBeltC()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.BELT;
+		String item_ability = Game_constants.CONSTITUTION;
+		int item_point=1;
+		int constitution=11;
+		
+		CharacterModel item_character=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+		
+
+
+		item_character.setItems(items_cha);
+		item_character.getRawAbilityScores().setConstitution(10);
+		
+
+		item_character.calculateModifires();
+		
+		Assert.assertEquals(item_character.getAbilityScores().getConstitution(),constitution);
+		
+	}
+	
+	@Test
+	public void testAbilityAccordingToItemWornBoot()
+	{
+		int item_id=1;
+		String item_name="abc";
+		String itemtype = Game_constants.BOOTS;
+		String item_ability = Game_constants.DEXTERITY;
+		int item_point=1;
+		int dexterity=11;
+		
+		CharacterModel item_character=new CharacterModel();
+		
+		ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
+		
+		ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
+		items_cha.add(item);
+
+
+
+		item_character.setItems(items_cha);
+		item_character.getRawAbilityScores().setDexterity(10);
+		
+
+		item_character.calculateModifires();
+		
+		Assert.assertEquals(item_character.getAbilityScores().getDexterity(),dexterity);
+		
+	}
 	
 	
 
@@ -36,67 +288,6 @@ public class TestCharacter {
 			// Given
 			// When
 			// Then
-			int score,modifier=0;
-			CharacterModel characterability=new CharacterModel();
-			
-			
-			characterability.calculateAbilityScores();
-			score=characterability.getAbilityScores().getStraight();
-//			System.out.println(characterability.getAbilityScores().getStraight());
-
-			if (score == 1) {
-				modifier = -5;
-			} else if (score == 2 || score == 3) {
-				modifier = -4;
-			} else if (score == 4 || score == 5) {
-				modifier = -3;
-			} else if (score == 6 || score == 7) {
-				modifier = -2;
-			} else if (score == 8 || score == 9) {
-				modifier = -1;
-			} else if (score == 10 || score == 11) {
-				modifier = 0;
-			} else if (score == 12 || score == 13) {
-				modifier = 1;
-			} else if (score == 14 || score == 15) {
-				modifier = 2;
-			} else if (score == 16 || score == 17) {
-				modifier = 3;
-			} else if (score == 18 || score == 19) {
-				modifier = 4;
-			} else if (score == 20 || score == 21) {
-				modifier = 5;
-			} else if (score == 22 || score == 23) {
-				modifier = 6;
-			} else if (score == 24 || score == 25) {
-				modifier = 7;
-			} else if (score == 26 || score == 27) {
-				modifier = 8;
-			} else if (score == 28 || score == 29) {
-				modifier = 9;
-			} else if (score == 30 || score == 31) {
-				modifier = 10;
-			} else if (score == 32 || score == 33) {
-				modifier = 11;
-			} else if (score == 34 || score == 35) {
-				modifier = 12;
-			} else if (score == 36 || score == 37) {
-				modifier = 13;
-			} else if (score == 38 || score == 39) {
-				modifier = 14;
-			} else if (score == 40 || score == 41) {
-				modifier = 15;
-			} else if (score == 42 || score == 43) {
-				modifier = 16;
-			} else if (score == 44 || score == 45) {
-				modifier = 17;
-			} else if (score == 46 || score == 47) {
-				modifier = 18;
-			}
-			characterability.calculateModifires();
-			
-
-		//	System.out.println(characterability.getAbilityScores().getStraight());
 			
 
 		}
@@ -150,322 +341,7 @@ public class TestCharacter {
 
 		}
 		
-		/**
-		 * Test character’s abilities according to items worn 
-		 */
-		@Test
-		public void testAbilityAccordingToItemWornHelmetI()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.HELMET;
-			String item_ability = Game_constants.INTELLIGENCE;
-			int item_point=5;
-			int intelligence=17;
-			
-			CharacterModel itemcharacter=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			itemcharacter.setItems(items_cha);
-			itemcharacter.getAbilityScores().setIntelligence(10);
-			
-			System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			itemcharacter.calculateModifires();
-			System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-			System.out.println(ability_int.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getIntelligence(),intelligence);
-			
-		}
 		
-		@Test
-		public void testAbilityAccordingToItemWornHelmetW()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.HELMET;
-			String item_ability = Game_constants.WISDOM;
-			int item_point=5;
-			int wisdom=17;
-		
-			CharacterModel itemcharacter=new CharacterModel();
-
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-		
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-			itemcharacter.setItems(items_cha);
-			itemcharacter.getAbilityScores().setWisdom(10);
-			
-			System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			itemcharacter.calculateModifires();
-			System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-			System.out.println(ability_int.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getWisdom(),wisdom);
-			
-		}
-		
-		@Test
-		public void testAbilityAccordingToItemWornRingS()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.RING;
-			String item_ability = Game_constants.STRENGTH;
-			int item_point=5;
-			int strength=17;
-			
-			CharacterModel itemcharacter=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			itemcharacter.setItems(items_cha);
-			itemcharacter.getAbilityScores().setstrength(10);
-			
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			itemcharacter.calculateModifires();
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-		//	System.out.println(abilityint.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getStraight(),strength);
-			
-		}
-		
-		@Test
-		public void testAbilityAccordingToItemWornRingC()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.RING;
-			String item_ability = Game_constants.CONSTITUTION;
-			int item_point=5;
-			int constitution=17;
-			
-			CharacterModel itemcharacter=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> itemscha = new ArrayList<ItemModel>();
-			itemscha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			itemcharacter.setItems(itemscha);
-			itemcharacter.getAbilityScores().setConstitution(10);
-			
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			itemcharacter.calculateModifires();
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-		//	System.out.println(abilityint.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getConstitution(),constitution);
-			
-		}
-		
-		@Test
-		public void testAbilityAccordingToItemWornRingW()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.RING;
-			String item_ability = Game_constants.WISDOM;
-			int item_point=5;
-			int wisdom=17;
-			
-			CharacterModel itemcharacter=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			itemcharacter.setItems(items_cha);
-			itemcharacter.getAbilityScores().setWisdom(10);
-			
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			itemcharacter.calculateModifires();
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-		//	System.out.println(abilityint.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getWisdom(),wisdom);
-			
-		}
-		
-		@Test
-		public void testAbilityAccordingToItemWornRingCh()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.RING;
-			String item_ability = Game_constants.CHARISMA;
-			int item_point=5;
-			int charisma=17;
-			
-			CharacterModel item_character=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			item_character.setItems(items_cha);
-			item_character.getAbilityScores().setCharisma(10);
-			
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			item_character.calculateModifires();
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-		//	System.out.println(abilityint.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getCharisma(),charisma);
-			
-		}
-		
-		@Test
-		public void testAbilityAccordingToItemWornBeltS()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.BELT;
-			String item_ability = Game_constants.STRENGTH;
-			int item_point=5;
-			int strength=17;
-			
-			CharacterModel item_character=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			item_character.setItems(items_cha);
-			item_character.getAbilityScores().setstrength(10);
-			
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			item_character.calculateModifires();
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-		//	System.out.println(abilityint.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getStraight(),strength);
-			
-		}
-		
-		@Test
-		public void testAbilityAccordingToItemWornBeltC()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.BELT;
-			String item_ability = Game_constants.CONSTITUTION;
-			int item_point=5;
-			int constitution=17;
-			
-			CharacterModel item_character=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			item_character.setItems(items_cha);
-			item_character.getAbilityScores().setConstitution(10);
-			
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			item_character.calculateModifires();
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-		//	System.out.println(abilityint.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getConstitution(),constitution);
-			
-		}
-		
-		@Test
-		public void testAbilityAccordingToItemWornBoot()
-		{
-			int item_id=1;
-			String item_name="abc";
-			String itemtype = Game_constants.BOOTS;
-			String item_ability = Game_constants.DEXTERITY;
-			int item_point=5;
-			int dexterity=17;
-			
-			CharacterModel item_character=new CharacterModel();
-			
-			ItemModel item=new ItemModel(item_id,item_name,item_point,itemtype,item_ability); 
-			
-			ArrayList<ItemModel> items_cha = new ArrayList<ItemModel>();
-			items_cha.add(item);
-			
-
-			AbilityScoresModel ability_int=new AbilityScoresModel();
-
-			item_character.setItems(items_cha);
-			item_character.getAbilityScores().setDexterity(10);
-			
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-
-			item_character.calculateModifires();
-		//	System.out.println(itemcharacter.getAbilityScores().getIntelligence());
-			
-		//	System.out.println(abilityint.getStraight());
-			
-					
-			Assert.assertEquals(ability_int.getDexterity(),dexterity);
-			
-		}
 	}
 
 
