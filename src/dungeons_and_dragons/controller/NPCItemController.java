ed3c91356adab67a75a234209ed60ef81b94214f
mihir <mihir@Mihir-Laptop>
2017-03-17 16:21:59 -0400

    fix enemy interaction in game play controller

--- src/dungeons_and_dragons/controller/NPCItemController.java
+++ src/dungeons_and_dragons/controller/NPCItemController.java
@@ -8,30 +8,38 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import dungeons_and_dragons.model.CharacterModel;
+import dungeons_and_dragons.model.GamePlayModel;
 import dungeons_and_dragons.model.ItemModel;
-import dungeons_and_dragons.view.CharacterInventoryView;
 import dungeons_and_dragons.view.NPCItemView;
 
 /**
  * @author Hirangi Naik
  *
  */
-public class NPCItemController implements ActionListener{
-	
+public class NPCItemController implements ActionListener {
+
 	private NPCItemView view;
 	public ArrayList<ItemModel> items;
 	public boolean isEnemy;
-	private GamePlayController controller;
+	private GamePlayModel model;
+
+	private CharacterModel character;
 
 	/**
 	 * 
 	 */
-	public NPCItemController(GamePlayController gamePlayController, ArrayList<ItemModel> items, boolean isEnemy) {
-		
-		this.controller=gamePlayController;
-		this.isEnemy=isEnemy;
-		this.items=items;
+	public NPCItemController(GamePlayModel gamePlayModel, ArrayList<ItemModel> items, boolean isEnemy,
+			CharacterModel character) {
+
+		this.model = gamePlayModel;
+		this.isEnemy = isEnemy;
+		this.items = items;
+
+		this.character = character;
 		this.view = new NPCItemView(this.items);
 		view.setActionListener(this);
 		this.view.setVisible(true);
@@ -39,15 +47,33 @@ public class NPCItemController implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource().equals(view.okButton)){
-			
-			List<ItemModel> items = this.view.itemList.getSelectedValuesList();
-			if ((items == null) || (items.size() < 1))
-				return;
-			this.controller.gamePlayModel.getCharacterModel().getBackPackItems().addAll(items);
-			this.view.dispose();
-		}
-			
+		if (e.getSource().equals(view.okButton)) {
+			if (isEnemy) {
+				List<ItemModel> items = this.view.itemList.getSelectedValuesList();
+				if ((items == null) || (items.size() < 1))
+					return;
+				// this.controller.gamePlayModel.getCharacterModel().getBackPackItems().addAll(items);
+				
+				
+				if (this.model.getCharacterModel().getBackPackItems().size() < 10) {
+					for (int i = 0; this.model.getCharacterModel().getBackPackItems().size() < 10 && i<items.size(); i++) {
+						this.model.getCharacterModel().getBackPackItems().add(items.get(i));
+						this.items.remove(items.get(i));
+					}
+				} else {
+					JOptionPane.showMessageDialog(new JFrame(),
+							"Sorry your backpack is full.So cannot add any new Item");
+				}
+				this.view.dispose();
+			} else {
+				
+				List<ItemModel> items = this.view.itemList.getSelectedValuesList();
+				if ((items == null) || (items.size() < 1))
+					return;
+				
+				
+				
+			}
+		} 
 	}
-
 }
