7ad23c38fdf4bb3d90fce894122cccec26d78681
Hirangi <Hirangi@lenovo>
2017-03-21 15:29:32 -0400

    test suite and testcase

---
+++ src/dungeons_and_dragons/test/TestSuite.java
@@ -0,0 +1,16 @@
+package dungeons_and_dragons.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   TestCharacter.class,
+   TestItem.class,
+   TestMap.class
+   })
+
+public class TestSuite {
+
+}
