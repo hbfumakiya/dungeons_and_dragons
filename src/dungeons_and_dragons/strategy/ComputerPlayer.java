448e79d3140c16f5aa8d81eacc519523e55c9680
pujaramihir <mihirppujara@gmail.com>
2017-04-05 15:25:24 -0400

    changes in computer player and friendly npc

--- src/dungeons_and_dragons/strategy/ComputerPlayer.java
+++ src/dungeons_and_dragons/strategy/ComputerPlayer.java
@@ -14,19 +14,34 @@ public class ComputerPlayer implements Strategy {
 	@Override
 	public void move(GamePlayModel gamePlayModel) {
 		System.out.println("Computer Player Move");
-		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void attack(GamePlayModel gamePlayModel) {
 		System.out.println("Computer Player Attack");
-		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void interact(GamePlayModel gamePlayModel) {
 		System.out.println("Computer Player Interact");
-		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/dungeons_and_dragons/strategy/FriendlyNPC.java
+++ src/dungeons_and_dragons/strategy/FriendlyNPC.java
@@ -14,16 +14,34 @@ public class FriendlyNPC implements Strategy {
 	@Override
 	public void move(GamePlayModel gamePlayModel) {
 		System.out.println("Friendly NPC Move");
-		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void attack(GamePlayModel gamePlayModel) {
 		System.out.println("Friendly NPC Move");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void interact(GamePlayModel gamePlayModel) {
 		System.out.println("Friendly NPC Move");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
